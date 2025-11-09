79e70e235ec3e26da9586090c54a5c30af9448a0
Kramarukha Vladislav <Vladislav.Kramarukha@sitronics-kt.ru>
2025-11-09 21:36:31 +0400

    Test

--- diplom-core/src/renderer/src/App.vue
+++ diplom-core/src/renderer/src/App.vue
@@ -1,8 +1,16 @@
 <script setup>
-
 import Button from './components/Button.vue'
+
+function onCustomClick() {
+    alert('НЕ ПОШЕЛ НАХУЙ!')
+}
+
+function onCustomClick2() {
+    alert('ПОШЕЛ НАХУЙ!')
+}
 </script>
 
 <template>
-    <Button />
+    <Button label="Кнопка" @custom-click="onCustomClick"/>
+    <Button label="Кнопка 2" @custom-click="onCustomClick2" />
 </template>
--- diplom-core/src/renderer/src/components/Button.vue
+++ diplom-core/src/renderer/src/components/Button.vue
@@ -1,6 +1,10 @@
 <template>
-    <button class="bg-blue-500 px-4 py-2 rounded-lg">Кнопка</button>
+    <button class="bg-blue-500 px-4 py-2 rounded-lg cursor-pointer" @click="emit('customClick')">
+        {{ props.label }}
+    </button>
 </template>
 
 <script setup>
+const emit = defineEmits(['customClick'])
+const props = defineProps(['label'])
 </script>
