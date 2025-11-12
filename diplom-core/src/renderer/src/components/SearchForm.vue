632af7c1fbdd4e77a265edca8980abbfcc086b47
Dismoral5 <ggggamer35@mail.ru>
2025-11-12 22:42:36 +0300

    мои мысли

--- diplom-core/src/renderer/src/components/SearchForm.vue
+++ diplom-core/src/renderer/src/components/SearchForm.vue
@@ -3,17 +3,39 @@
         <template #header> Данные по помещению </template>
 
         <template #default>
-            <NForm>
-                <NFormItem label="Метраж">
-                    <NInputNumber placeholder="Введите данные" :bordered="false" />
-                </NFormItem>
-            </NForm>
+                <NForm>
+                    <NFormItem label="Площадь помещения">
+                        <NInputNumber  placeholder="Введите данные" :bordered="false" />
+                    </NFormItem>
+                </NForm>
+                <NForm>
+                    <NFormItem label="Толщина стен">
+                        <NInputNumber  placeholder="Введите данные" :bordered="false" />
+                    </NFormItem>
+                </NForm>
+                <NForm>
+                    <NFormItem label="Наличие САЗ">
+                        <input type="checkbox" id="yes" name="yes" aria-checked="true" :bordered="false" />
+                        <label for="yes">Да</label>
+                        <input type="checkbox" id="no" name="no" aria-checked="true" :bordered="false" />
+                        <label for="no">Нет</label>
+                    </NFormItem>
+                </NForm>
+                <NForm>
+                    <NFormItem label="Перечень документов для работы Кгр">
+                        <NInput  placeholder="Введите данные" :bordered="false" />
+                    </NFormItem>
+                </NForm>
+           
         </template>
 
         <template #action>
-            <NButton tertiary>Проверить</NButton>
+            <NButton tertiary >Проверить </NButton>
         </template>
     </NCard>
 </template>
 
-<script setup></script>
+<script setup>
+import { NCheckbox, NCheckboxGroup, NForm, NFormItem, NInput, NInputGroupLabel, NInputOtp, NMenu } from 'naive-ui';
+
+</script>
