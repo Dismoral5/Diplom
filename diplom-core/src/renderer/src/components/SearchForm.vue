15e740b30a7b8b3b76c9214b338371ec736fb440
Kramarukha Vladislav <Vladislav.Kramarukha@sitronics-kt.ru>
2025-11-13 00:26:11 +0400

    сделал по красоте 2.0

--- diplom-core/src/renderer/src/components/SearchForm.vue
+++ diplom-core/src/renderer/src/components/SearchForm.vue
@@ -3,39 +3,38 @@
         <template #header> Данные по помещению </template>
 
         <template #default>
-                <NForm>
-                    <NFormItem label="Площадь помещения">
-                        <NInputNumber  placeholder="Введите данные" :bordered="false" />
-                    </NFormItem>
-                </NForm>
-                <NForm>
-                    <NFormItem label="Толщина стен">
-                        <NInputNumber  placeholder="Введите данные" :bordered="false" />
-                    </NFormItem>
-                </NForm>
-                <NForm>
-                    <NFormItem label="Наличие САЗ">
-                        <input type="checkbox" id="yes" name="yes" aria-checked="true" :bordered="false" />
-                        <label for="yes">Да</label>
-                        <input type="checkbox" id="no" name="no" aria-checked="true" :bordered="false" />
-                        <label for="no">Нет</label>
-                    </NFormItem>
-                </NForm>
-                <NForm>
-                    <NFormItem label="Перечень документов для работы Кгр">
-                        <NInput  placeholder="Введите данные" :bordered="false" />
-                    </NFormItem>
-                </NForm>
-           
+            <NForm>
+                <NFormItem label="Площадь помещения">
+                    <NInputNumber placeholder="Введите данные" :bordered="false">
+                        <template #suffix> м<sup>2</sup> </template>
+                    </NInputNumber>
+                </NFormItem>
+
+                <NFormItem label="Толщина стен">
+                    <NInputNumber placeholder="Введите данные" :bordered="false">
+                        <template #suffix> м </template>
+                    </NInputNumber>
+                </NFormItem>
+
+                <NFormItem label="Наличие САЗ">
+                    <NSpace>
+                        <NCheckbox label="Да" />
+                        <NCheckbox label="Нет" />
+                    </NSpace>
+                </NFormItem>
+
+                <NFormItem label="Перечень документов для работы Кгр">
+                    <NSelect multiple :bordered="false" placeholder="Выберите из списка" />
+                </NFormItem>
+            </NForm>
         </template>
 
         <template #action>
-            <NButton tertiary >Проверить </NButton>
+            <NButton tertiary>Проверить </NButton>
         </template>
     </NCard>
 </template>
 
 <script setup>
-import { NCheckbox, NCheckboxGroup, NForm, NFormItem, NInput, NInputGroupLabel, NInputOtp, NMenu } from 'naive-ui';
-
+import { NCheckbox, NForm, NFormItem } from 'naive-ui'
 </script>
